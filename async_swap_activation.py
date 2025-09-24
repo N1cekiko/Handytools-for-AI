14a255e5a172d3f680ae5b2d9e279db74af144c1
HMH <38829641+N1cekiko@users.noreply.github.com>
2025-09-24 16:01:07 +0800

    Update async_swap_activation.py readme

--- async_swap_activation.py
+++ async_swap_activation.py
@@ -19,3 +19,9 @@ for idx， decode_layer in enumerate(self.layers):
             position_embeddings=position_embeddings,
             seq_ctx=seq_ctx
           )
+
+
+# h2d_stream/d2h_stream: 执行swap操作的stream，一般不用计算和通信的流，单独创建一条流，实现一步操作
+# block idx：当前layer的编号，前向计算是，函数会在i+1层开始计算的时候触发第i层激活值得H2D；反向计算时，会在第i+1层计算时，触发第i层的d2H
+# depth：模型总层数
+# custom_check_fn:可以自己定义校验函数，满足该校验函数的激活值才会被offload
